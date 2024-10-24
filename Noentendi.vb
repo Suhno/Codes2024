48676b27fb95d9d6ee311dd98ba4e092a2d2d85e
Suhno <131544360+Suhno@users.noreply.github.com>
2024-10-23 19:50:00 -0500

    Create Noentendi.vb

---
+++ Noentendi.vb
@@ -0,0 +1,9 @@
+Module HelloWorld
+    Sub Main()
+        ' Display message on the console
+        Console.WriteLine("Hello, World!")
+        
+        ' Wait for user input before closing
+        Console.ReadLine()
+    End Sub
+End Module
